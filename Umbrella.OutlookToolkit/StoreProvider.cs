3a731f714e4268a319a366b27d464810e3667404
michaelfouks <michaelfouks@hotmail.com>
2023-03-12 22:54:34 -0400

    Started working on StoreProvider.

--- Umbrella.OutlookToolkit/StoreProvider.cs
+++ Umbrella.OutlookToolkit/StoreProvider.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Outlook;
 
 namespace Umbrella.OutlookToolkit
 {
     public class StoreProvider
     {
+        private readonly string storeFileName;
+
+        private StoreProvider() { }
+
+        public StoreProvider(string storeFileNameIn)
+        {
+            storeFileName = storeFileNameIn ?? throw new ArgumentNullException(nameof(storeFileNameIn));
+        }
     }
 }
